d4c7958cad9e28fb4593e945358e1361fb96c371
aghimire <aghimire@dhcp-10-134-234-49.uawifi.arizona.edu>
2019-01-16 11:27:45 -0700

    Jan 16,2019, First lab

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,7 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Aayush Ghimire
+ * CS 335 lab1
  * 
  * 
  */
@@ -8,7 +9,7 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello CSC 335, I'm Aayush Ghimire.");
+		System.out.println("Hello CSC 335, I'm Aayush Ghimire");
 	}
 
 }
